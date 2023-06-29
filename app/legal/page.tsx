230edd213b78cb4360ca970cb62c78ed449254fa
murderteeth <murdertxxth@gmail.com>
2023-06-29 19:28:51 +0000

    Better legal

--- app/legal/page.tsx
+++ app/legal/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
     <div className="flex flex-col items-center justify-center gap-4">
       <Image priority={true} src={'/chimp.png'} alt={'ahoy'} width={400} height={400} />
       <div className="text-4xl">yGptPlugin</div>
-      <div>Legal - We need "legal" here..</div>
+      <div>{'Legal - We need "legal" here. Can someone call legal?..'}</div>
     </div>
 
   </main>
